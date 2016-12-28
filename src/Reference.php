856d4614535552696edd278235bf0b8fb08cd2c0
Joel Wurtz <jwurtz@jolicode.com>
2016-12-28 17:11:20 +0100

    Allow reference to be resolved, remove public api (BC BREAK)

--- src/Reference.php
+++ src/Reference.php
@@ -2,184 +2,84 @@
 
 namespace Joli\Jane\Runtime;
 
+use League\Uri\Schemes\Generic\AbstractUri;
+use League\Uri\Schemes\Http;
+use League\Uri\UriParser;
+use Rs\Json\Pointer;
+
 /**
  * Deal with a Json Reference
  */
 class Reference
 {
     /**
-     * @var string
-     */
-    private $scheme;
-
-    /**
-     * @var string
-     */
-    private $host;
-
-    /**
-     * @var integer
-     */
-    private $port;
-
-    /**
-     * @var string
-     */
-    private $path;
-
-    /**
-     * @var string
-     */
-    private $query;
-
-    /**
-     * @var string
-     */
-    private $fragment;
-
-    /**
-     * @var string
-     */
-    private $reference;
-
-    /**
-     * @var mixed
-     */
-    private $currentSchema;
-
-    /**
      * @var mixed
      */
     private $resolved;
 
     /**
-     * @param $ref
-     * @param mixed $currentSchema
+     * @var AbstractUri
      */
-    public function __construct($ref, $currentSchema)
-    {
-        $this->reference = $ref;
-        $this->scheme    = parse_url($ref, PHP_URL_SCHEME);
-        $this->host      = parse_url($ref, PHP_URL_HOST);
-        $this->port      = parse_url($ref, PHP_URL_PORT);
-        $this->path      = parse_url($ref, PHP_URL_PATH);
-        $this->query     = parse_url($ref, PHP_URL_QUERY);
-        $this->fragment  = parse_url($ref, PHP_URL_FRAGMENT);
-        $this->currentSchema = $currentSchema;
+    private $uri;
 
-        // Differentiate, root fragment and none existent fragment
-        if ($this->fragment === null && preg_match('/#/', $ref)) {
-            $this->fragment = '';
+    /**
+     * @var string
+     */
+    private $original;
+
+    /**
+     * @param string $reference
+     * @param string $origin
+     */
+    public function __construct($reference, $origin)
+    {
+        $uriParse = new UriParser();
+        $currentSchemaPath = $uriParse->parse($origin);
+        $referencePath = parse_url($reference);
+        $mergedPath = array_merge($currentSchemaPath, $referencePath);
+
+        $this->original = $reference;
+        $this->uri = Http::createFromComponents($mergedPath);
+    }
+
+    /**
+     * Resolve a JSON Reference
+     *
+     * @param callable $deserializeCallback
+     *
+     * @return mixed Return the json value (deserialized) referenced
+     */
+    public function resolve(callable $deserializeCallback = null)
+    {
+        if (null === $deserializeCallback) {
+            $deserializeCallback = function ($data) { return $data; };
         }
+
+        if ($this->resolved === null) {
+            $this->resolved = $this->doResolve();
+        }
+
+        return $deserializeCallback($this->resolved);
+    }
+
+    /**
+     * Resolve a JSON Reference for a Schema
+     *
+     * @return mixed Return the json value referenced
+     */
+    protected function doResolve()
+    {
+        $json = file_get_contents($this->uri->withFragment(''));
+        $pointer = new Pointer($json);
+
+        return $pointer->get($this->uri->getFragment());
     }
 
     /**
      * @return string
-     */
-    public function getFragment()
-    {
-        return $this->fragment;
-    }
-
-    /**
-     * @return string
-     */
-    public function getHost()
-    {
-        return $this->host;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPath()
-    {
-        return $this->path;
-    }
-
-    /**
-     * @return int
-     */
-    public function getPort()
-    {
-        return $this->port;
-    }
-
-    /**
-     * @return string
-     */
-    public function getQuery()
-    {
-        return $this->query;
-    }
-
-    /**
-     * @return string
-     */
-    public function getScheme()
-    {
-        return $this->scheme;
-    }
-
-    /**
-     * @return mixed
      */
     public function getReference()
     {
-        return $this->reference;
-    }
-
-    /**
-     * Whether this reference is relative to the current document
-     *
-     * @return bool
-     */
-    public function isRelative()
-    {
-        return $this->host === null;
-    }
-
-    /**
-     * Whether this reference is in the current document
-     *
-     * @return bool
-     */
-    public function isInCurrentDocument()
-    {
-        return $this->isRelative() && $this->path === null;
-    }
-
-    /**
-     * Whether this reference has fragment
-     *
-     * @return bool
-     */
-    public function hasFragment()
-    {
-        return $this->fragment !== null;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCurrentSchema()
-    {
-        return $this->currentSchema;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getResolved()
-    {
-        return $this->resolved;
-    }
-
-    /**
-     * @param mixed $resolved
-     */
-    public function setResolved($resolved)
-    {
-        $this->resolved = $resolved;
+        return $this->original;
     }
 }
